28df3ae15db89121491c57ad77fc0eb7706a1229
alanlhy94 <115515042+alanlhy94@users.noreply.github.com>
2022-12-09 07:32:56 -0800

    Adding thmynleaf tempalte layout and navbar to the project

--- src/EmployeeControllerTest.java
+++ src/EmployeeControllerTest.java
@@ -1,14 +1,25 @@
 import javat.Empdao;
+import javat.Employee;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.SQLException;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
 
 @RunWith(MockitoJUnitRunner.class)
-class EmployeeControllerTest {
+public class EmployeeControllerTest {
 
     @Mock
     JdbcTemplate mockJdbcTemplate;
@@ -17,8 +28,27 @@ class EmployeeControllerTest {
     Empdao empdao;
 
     String employeeId;
-    String dob;
+    Date dob;
     String firstname;
     String lastname;
+    String gender;
+    Date hireDate;
+    float hourRate;
+
+    @Test
+    public void addData(){
+
+        Employee a = new Employee();
+
+        lenient().when(mockJdbcTemplate.update(anyString(),anyString(),anyString(),anyString(),anyString(),anyString(),anyString(),anyString())).thenReturn(1);
+        empdao.insertData(a);
+        ArgumentCaptor<Object> varArgs = ArgumentCaptor.forClass(Object.class);
+        ArgumentCaptor<String> strArgs = ArgumentCaptor.forClass(String.class);
+
+        verify(mockJdbcTemplate).update(strArgs.capture(), varArgs.capture(),varArgs.capture(),varArgs.capture(),varArgs.capture(),varArgs.capture(),varArgs.capture(),varArgs.capture());
+
+        assertEquals(employeeId, varArgs.getAllValues().get(0));
+    }
+
 
 }
